68918aad4cd53550091f7f075193759cde699c5c
wittavat <Wittavat.s@ku.th>
2022-01-31 16:27:55 +0700

    Setup Email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'https://saas-project-app01ruby.herokuapp.com/', :protocol => 'https'}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
